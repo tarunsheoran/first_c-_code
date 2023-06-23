b0397beb7dc6565e820548a8a133a0705a4c3a1f
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-06-23 21:19:43 +0530

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a=12;
+    cout<<"Size of int " <<sizeof(a)<<endl;
+
+    float b;
+        cout<<"Size of float " <<sizeof(b)<<endl;
+
+char c;
+    cout<<"Size of char " <<sizeof(c)<<endl;
+
+    bool d;
+        cout<<"Size of boolean " <<sizeof(d)<<endl;
+        
+
+
+return 0;
+}
